342ca3a41099590bd4e49c6575af835b349d2a84
Adam Jordens <ajordens@netflix.com>
2016-09-25 21:54:11 -0700

    Initial project commit

---
+++ gate-netflix/src/main/groovy/com/netflix/spinnaker/internal/controllers/PingController.groovy
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2015 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.internal.controllers
+
+import groovy.transform.CompileStatic
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RestController
+
+@CompileStatic
+@RequestMapping("/ping")
+@RestController
+class PingController {
+  @RequestMapping(method = RequestMethod.GET)
+  String ping() {
+    return "pong"
+  }
+}
